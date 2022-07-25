21b1d73e5eed261d926cfea77b54d151d5a3cd16
maciej.filanowicz <mfilon13@gmail.com>
2022-07-25 22:26:19 +0200

    add model

--- download_processed_data.sh
+++ download_processed_data.sh
@@ -8,14 +8,14 @@ names=(
 #splitted_processed_0_25_50.zip,
 #splitted_processed_patched.zip,
 #merged_test_images.zip
-processed_base.zip
+processed_base2.zip
 )
 
 urls=(
 #"https://onedrive.live.com/download?cid=389519B65EF435AE&resid=389519B65EF435AE%2126961&authkey=ACoIKEV-MSBQ-us",
 #"https://onedrive.live.com/download?cid=389519B65EF435AE&resid=389519B65EF435AE%2127000&authkey=AMNmXIBP_tjMeRo",
 #"https://onedrive.live.com/download?cid=389519B65EF435AE&resid=389519B65EF435AE%2127001&authkey=AJdkPhg7kZpdA_E"
-"https://onedrive.live.com/download?cid=389519B65EF435AE&resid=389519B65EF435AE%2120604&authkey=ABjTniFybb4T6VA"
+"https://onedrive.live.com/embed?cid=389519B65EF435AE&resid=389519B65EF435AE%2127003&authkey=AJBYgKoG5IPibHg"
 )
 
 # Download all the files
